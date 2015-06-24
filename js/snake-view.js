270de81f00fe6a7a6474a72105af0293293c398e
Karen Ling <karensling@gmail.com>
2015-06-23 20:45:44 -0700

    Set up view board.

--- js/snake-view.js
+++ js/snake-view.js
@@ -3,5 +3,20 @@
     window.SnakeGame = {};
   }
 
-  
+  var View = SnakeGame.View = function ($el) {
+    this.$el = $el;
+    this.board = SnakeGame.Board(); // from other file
+    this.setupBoard();
+  };
+
+  View.prototype.setupBoard = function () {
+    for(i = 0; i <= 20; i++) {
+      var $row = $('<div class="row">');
+      for(j = 0; j <= 20; j++) {
+        $row.append($('<div>').addClass("cell"));
+      }
+      this.$el.append($row);
+    }
+  };
+
 })();
